25406491c8813662c7ae2d06735584c0b6500b38
DESKTOP-CJH2Q8D\ADMIN <kudo02012004@gmail.com>
2025-08-27 13:16:38 +0700

    initial commit

---
+++ drizzle.config.js
@@ -0,0 +1,7 @@
+import { env } from "./src/config/env.js";
+export default{
+    schema: "./src/db/schema.js",
+    out: "./src/db/migrations",
+    dialect: "postgresql",
+    dbCredentials: {url: env.DATABASE_URL},
+};
